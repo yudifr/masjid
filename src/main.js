476de9588fbdb10373d094badf7fee2347f454a2
Yudi Fathurrahman <yudi@tsp.dev>
2022-04-23 10:57:39 +0700

    file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import { createApp } from "vue";
-import router from "./router";
 import App from "./App.vue";
-const app = createApp(App);
 
-app.use(router).mount("#app");
+createApp(App).mount("#app");
